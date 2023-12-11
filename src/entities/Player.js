ab7f5b83dd5166b3877f6e6ca8b43d2906cd9469
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-12-11 17:58:38 +0100

    Add Player object

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,3 +1,29 @@
 class Player {
-  constructor() {}
+  #userName = '';
+
+  #hitLog = [];
+
+  constructor(userName) {
+    this.#userName = userName;
+  }
+
+  logHit(coord) {
+    this.#hitLog.push(coord);
+  }
+
+  getHitLog() {
+    const hitLogCopy = this.#hitLog.slice();
+
+    return hitLogCopy;
+  }
+
+  clearHitLog() {
+    this.#hitLog = [];
+  }
+
+  getUserName() {
+    return this.#userName
+  };
 }
+
+export default Player;
