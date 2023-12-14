b0155f4040937225f32c4691929005f48d3e57ef
Siriusxz <114768817+Siriuszx@users.noreply.github.com>
2023-12-14 14:18:51 +0100

    Test GameboardDataBuilder object

---
+++ src/test/entities/GameboardDataBuilder.test.js
@@ -0,0 +1,19 @@
+import { test, expect, describe, jest } from "@jest/globals";
+import GameBoardDataBuilder from "../../entities/DataPointContainers/GameboardDataBuilder";
+
+describe('valid data creation', () => {
+    test('test data creation/retrieval', () => {
+        const mockGetBoard = jest.fn(() => []);
+
+        const builder = new GameBoardDataBuilder();
+        const gameboardData = builder
+            .setBoardData(mockGetBoard())
+            .setPlayerUserName('Player One')
+            .setAllShipsSunk(false)
+            .build();
+
+        expect(gameboardData.boardData).toHaveLength(0);
+        expect(gameboardData.playerUserName).toBe('Player One');
+        expect(gameboardData.allShipsSunk).toBeFalsy();
+    });
+});
